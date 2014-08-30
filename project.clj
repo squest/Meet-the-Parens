d9324a4aa79377a396ecdd3eb659f282f45d9459
Paul Sabda <sabdaps@gmail.com>
2014-08-30 18:41:38 +0700

    first commit

---
+++ project.clj
@@ -0,0 +1,68 @@
+(defproject
+  zencoding "0.1.0"
+  :repl-options  {:init-ns zencoding.repl}
+  :dependencies  [[ring-server "0.3.1"]
+                  [noir-exception "0.2.2"]
+                  [org.clojure/clojurescript "0.0-2280"]
+                  [environ "0.5.0"]
+                  [com.taoensso/timbre "3.2.1"]
+                  [markdown-clj "0.9.47"]
+                  [im.chit/cronj "1.0.1"]
+                  [org.clojure/clojure "1.6.0"]
+                  [com.taoensso/tower "2.0.2"]
+                  [http-kit "2.1.18"]
+                  [cljs-ajax "0.2.6"]
+                  [secretary "1.2.0"]
+                  [selmer "0.6.9"]
+                  [lib-noir "0.8.5"]
+                  [com.cemerick/url "0.1.1"]
+                  [com.ashafa/clutch "0.4.0-RC1"]]
+  :cljsbuild  {:builds
+               [{:source-paths ["src-cljs"],
+                 :compiler
+                 {:pretty-print true,
+                  :output-dir "resources/public/js/",
+                  :source-map true,
+                  :output-to "resources/public/js/app.js",
+                  :optimizations :none},
+                 :id "dev"}
+                {:source-paths ["src-cljs"],
+                 :compiler
+                 {:pretty-print false,
+                  :closure-warnings {:non-standard-jsdoc :off},
+                  :output-to "resources/public/js/app.js",
+                  :output-wrapper false,
+                  :optimizations :none},
+                 :id "prod"}]}
+  :ring {:handler zencoding.handler/app,
+         :init zencoding.handler/init,
+         :destroy zencoding.handler/destroy}
+  :profiles {:uberjar {:aot :all},
+             :production
+             {:ring
+              {:open-browser? false, :stacktraces? false, :auto-reload? false}},
+             :dev
+             {:dependencies
+              [[ring-mock "0.1.5"]
+               [ring/ring-devel "1.3.0"]
+               [pjstadig/humane-test-output "0.6.0"]],
+              :injections
+              [(require 'pjstadig.humane-test-output)
+               (pjstadig.humane-test-output/activate!)],
+              :env {:dev true}}}
+  :url
+  "http://example.com/FIXME"
+  :main
+    zencoding.core
+  :jvm-opts
+  ["-server"]
+  :plugins
+  [[lein-ring "0.8.11"]
+   [lein-environ "1.0.0"]
+   [lein-cljsbuild "1.0.3"]
+   [lein-expectations "0.0.8"]
+   [codox "0.8.10"]
+   [lein-autoexpect "1.2.2"]]
+  :description
+  "FIXME: write description"
+  :min-lein-version "2.0.0")
---
+++ src/zencoding/dbase/config.clj
@@ -0,0 +1,35 @@
+(ns zencoding.dbase.config
+  (:require [cemerick.url :as curl]
+            [com.ashafa.clutch :as cl]))
+
+(def db-config
+  "The data for db configuration, coming from an edn file"
+  (read-string (slurp "resources/config.edn")))
+
+(defn configure
+  "Returns the complete url with username and password to access the
+  database"
+  [which-db]
+  (if (= :local which-db)
+    (->> db-config
+         :local-couch
+         :dbname)
+    (-> (curl/url "https://zenius.cloudant.com/zencode")
+        (assoc :username (->> db-config
+                              :cloudant
+                              :key)
+               :password (->> db-config
+                              :cloudant
+                              :password))
+        str)))
+
+(def cdb
+  "The actual db used throughout the application"
+  (configure :online))
+
+(defn test-input
+  "Some test to see if the setup is working"
+  [db data]
+  (cl/put-document cdb
+                   (merge {:ctype "_test"}
+                          data)))
