f93fed99dd8afb6649929aa2514ba46675c840c3
Paul Sabda <sabdaps@gmail.com>
2014-09-14 00:34:08 +0700

    fixed somethings

--- src/zencoding/dbase/config.clj
+++ src/zencoding/dbase/config.clj
@@ -4,7 +4,8 @@
             [zencoding.handler :as handler]
             [com.stuartsierra.component :as com]))
 
-;; ctypes : "_test" "course" "tutorial" "user" "admin"
+;; ctypes : "_test" "category"  "course" "tutorial"
+;; "user" "admin"
 ;; "problem" "answer" "_id"
 
 (def dbconf (com/start handler/zencoding-app))
--- src/zencoding/handler.clj
+++ src/zencoding/handler.clj
@@ -11,59 +11,59 @@
             [selmer.parser :as parser]
             [environ.core :refer [env]]
             [cronj.core :as cronj]
-						[cemerick.url :as curl]
-						[questdb.core :as qdb]
-						[com.stuartsierra.component :as com]))
+            [cemerick.url :as curl]
+            [questdb.core :as qdb]
+            [com.stuartsierra.component :as com]))
 
 (defrecord ZenCoding [fname whichcouch whichquest]
-	com/Lifecycle
+  com/Lifecycle
 
-	(start [this]
-		(let [conf (read-string (slurp "config.edn"))
-					couch-config (get conf whichcouch)
-					couch (assoc (curl/url (:url couch-config) (:dbname couch-config))
-									:username (:username couch-config)
-									:password (:password couch-config))
-					quest (get conf whichquest)]
-			(do (if (qdb/db-exists? quest)
-						nil
-						(qdb/create! quest))
-					(assoc this :cdb couch
-											:qdb quest
-											:git (:git conf)))))
+  (start [this]
+    (let [conf (read-string (slurp "config.edn"))
+          couch-config (get conf whichcouch)
+          couch (assoc (curl/url (:url
+                                  couch-config) (:dbname
+                                  couch-config))
+                  :username (:username couch-config)
+                  :password (:password couch-config))
+          quest (get conf whichquest)]
+      (do (if (qdb/db-exists? quest)
+            nil
+            (qdb/create! quest))
+          (assoc this :cdb couch
+                 :qdb quest
+                 :git (:git conf)))))
 
-	(stop [this]))
+  (stop [this]))
 
 (defn make-app [fname whichcouch whichquest]
-	(->ZenCoding fname whichcouch whichquest))
+  (->ZenCoding fname whichcouch whichquest))
 
 (def zencoding-app (make-app "config.edn"
-																:local-couch
-																:qdb-dev))
+                             :local-couch
+                             :qdb-dev))
 
 (defroutes base-routes
   (route/resources "/")
   (route/not-found "Not Found"))
 
 (defn init
-  "init will be called once when
-   app is deployed as a servlet on
-   an app server such as Tomcat
-   put any initialization code here"
+  "init will be called once when app is deployed as a servlet on an
+   app server such as Tomcat put any initialization code here"
   []
   (timbre/set-config!
-    [:appenders :rotor]
-    {:min-level :info
-     :enabled? true
-     :async? false ; should be always false for rotor
-     :max-message-per-msecs nil
-     :fn rotor/appender-fn})
+   [:appenders :rotor]
+   {:min-level :info
+    :enabled? true
+    :async? false                   ; should be always false for rotor
+    :max-message-per-msecs nil
+    :fn rotor/appender-fn})
 
-	(com/start zencoding-app)
+  (com/start zencoding-app)
 
   (timbre/set-config!
-    [:shared-appender-config :rotor]
-    {:path "zencoding.log" :max-size (* 512 1024) :backlog 10})
+   [:shared-appender-config :rotor]
+   {:path "zencoding.log" :max-size (* 512 1024) :backlog 10})
 
   (if (env :dev) (parser/cache-off!))
   ;;start the expired session cleanup job
@@ -71,24 +71,32 @@
   (timbre/info "zencoding started successfully"))
 
 (defn destroy
-  "destroy will be called when your application
-   shuts down, put any clean up code here"
+  "destroy will be called when your application shuts down, put any
+   clean up code here"
   []
   (timbre/info "zencoding is shutting down...")
   (cronj/shutdown! session-manager/cleanup-job)
   (timbre/info "shutdown complete!"))
 
 (def app (app-handler
-           ;; add your application routes here
-           [routes/home base-routes]
-           ;; add custom middleware here
-           :middleware (load-middleware)
-           ;; timeout sessions after 30 minutes
-           :session-options {:timeout (* 60 30)
-                             :timeout-response (redirect "/")}
-           ;; add access rules here
-           :access-rules []
-           ;; serialize/deserialize the following data formats
-           ;; available formats:
-           ;; :json :json-kw :yaml :yaml-kw :edn :yaml-in-html
-           :formats [:json-kw :edn]))
+          ;; add your application routes here
+          [routes/home base-routes]
+          ;; add custom middleware here
+          :middleware (load-middleware)
+          ;; timeout sessions after 30 minutes
+          :session-options {:timeout (* 60 30)
+                            :timeout-response (redirect "/")}
+          ;; add access rules here
+          :access-rules []
+          ;; serialize/deserialize the following data formats
+          ;; available formats: :json :json-kw :yaml :yaml-kw :edn
+          ;; :yaml-in-html
+          :formats [:json-kw :edn]))
+
+
+
+
+
+
+
+
